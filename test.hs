d29a3c52a27cbb1d0b2980a06ade07b1d1476fb0
1Regina <1Regina@github.com>
2020-09-10 23:04:32 +0800

    Chapt 11.3

--- test.hs
+++
@@ -1,5 +0,0 @@
-{-# LANGUAGE FlexibleInstances #-}
--- newtype IntSum' = IntSum' (Int, Int) deriving (Eq, Show, Num, TooMany)
--- instance (Num a, TooMany a) => TooMany (a, b) where tooMany (x, y) = tooMany (x + y)
-
-instance (Num a, TooMany a) => TooMany (a, a) where tooMany x = tooMany (fst x) || tooMany (snd x)
