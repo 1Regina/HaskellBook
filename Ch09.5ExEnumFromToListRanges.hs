5ac28ab71cbc31c2cf405fbaf7df698b6bc2e7a3
1Regina <46968488+1Regina@users.noreply.github.com>
2020-10-05 12:44:54 +0800

    enum from with ord a

--- Ch09.5ExEnumFromToListRanges.hs
+++ Ch09.5ExEnumFromToListRanges.hs
@@ -21,3 +21,20 @@ eftChar x y
   | x > y = []
   | x == y = [x]
   | otherwise = x : (eftChar (succ x) y)
+
+
+
+-- Basic function to use : 2 ways to write 
+eftBasic :: (Enum a, Ord a) => a -> a -> [a]
+eftBasic x y
+  | x > y  = []
+  | x == y = [x]
+  | otherwise = (x : (eftBasic (succ x) y))
+
+
+-- eftBasic' :: (Enum a, Ord a) => a -> a -> [a]
+-- eftBasic' x y = go x y []
+--   where go x y xs
+--           | x > y   = xs
+--           | x == y  = xs ++ (x : [])
+--           | x < y   = go (succ x) y (xs ++ [x])
